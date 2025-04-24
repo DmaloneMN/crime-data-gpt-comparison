c40ca6f8f1ff5186dfc20c92e431eb0a81462961
DmaloneMN <87717617+DmaloneMN@users.noreply.github.com>
2025-04-24 13:10:04 -0500

    Update main.tf

--- infra/aks/main.tf
+++ infra/aks/main.tf
@@ -7,18 +7,24 @@ resource "azurerm_resource_group" "crime_rg" {
   location = "East US"
 }
 
+resource "azurerm_key_vault" "crime_kv" {
+  name                = "crime-analysis-kv"
+  resource_group_name = azurerm_resource_group.crime_rg.name
+  location            = azurerm_resource_group.crime_rg.location
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "standard"
+}
+
 resource "azurerm_kubernetes_cluster" "crime_aks" {
   name                = "crime-analysis-aks"
   location            = azurerm_resource_group.crime_rg.location
   resource_group_name = azurerm_resource_group.crime_rg.name
   dns_prefix          = "crimeaks"
-
   default_node_pool {
     name       = "default"
     node_count = 2
     vm_size    = "Standard_B2s"
   }
-
   identity {
     type = "SystemAssigned"
   }
